67bdb924c5734079484f4091a619eb32102936c2
lhh <776334655@qq.com>
2020-04-26 14:20:35 +0800

    develop

--- brokers_test.go
+++ brokers_test.go
@@ -4,13 +4,14 @@ import (
 	"context"
 	"fmt"
 	"reflect"
+	"strconv"
 	"testing"
 )
 
-type TestMsg struct{}
+type TestMsg struct{ C int }
 
 func (t *TestMsg) GetName() string {
-	return "TestMsg"
+	return "TestMsg" + strconv.Itoa(t.C)
 }
 
 func (t *TestMsg) GetCtx() context.Context {
@@ -22,11 +23,23 @@ func (t *TestMsg) GetType() reflect.Type {
 }
 
 func TestSmq_Close(t *testing.T) {
-	GSmq.Register(context.Background(), func(msg *TestMsg) {
+	_ = GSmq.Register(context.Background(), func(msg *TestMsg) {
 		fmt.Println(msg.GetCtx(), msg.GetName(), msg.GetType())
 	})
-	GSmq.Send(context.Background(), &TestMsg{})
+	_ = GSmq.Send(context.Background(), &TestMsg{})
 	GSmq.Close()
+}
+
+func BenchmarkSmq_Register(b *testing.B) {
+	var smq = NewSmq()
+	defer smq.Close()
+	_ = smq.Register(context.Background(), func(msg *TestMsg) {
+		fmt.Println(msg.GetCtx(), msg.GetName(), msg.GetType())
+	})
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = smq.Send(context.Background(), &TestMsg{C: i})
+	}
 }
 
 func TestSmq_Close2(t *testing.T) {
